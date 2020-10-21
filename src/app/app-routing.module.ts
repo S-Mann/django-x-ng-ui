b8732d6c9d07be068174595658c64f079ba1833c
Sukhy <sukhindermann@gmail.com>
2020-10-21 11:59:29 +0000

    🚚 added metrics to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CustomHomeComponent } from './custom-home/custom-home.component';
 import { PageRequestsComponent } from './page-requests/page-requests.component';
+import { MetricsDashboardComponent } from './metrics-dashboard/metrics-dashboard.component';
 
 const routes: Routes = [
   { path: '', component: CustomHomeComponent },
   { path: 'requests', component: PageRequestsComponent },
+  { path: 'metrics', component: MetricsDashboardComponent },
 ];
 
 @NgModule({
